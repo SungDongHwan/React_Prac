9c019ebabd1c55297844976f7944311677958580
성동환 <122775098+SungDongHwan@users.noreply.github.com>
2023-02-13 02:44:06 +0900

    ver 1.2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-import Taccident from './05/Taccident';
 import Gal from './06/Gal'
-import Mycom from './07/Mycom';
-import Myform from './08/Myform';
+
+
 const App=()=> {
   return (
-  //  <div>
-  //    <Frcst />
-  //  </div>
-  //<Taccident/>
-  //  <Mycom />
    <Gal/>
-   //<Myform/>
       );
 }
 
