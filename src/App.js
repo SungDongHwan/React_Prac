f6d6df4bac150fd0c1b7b9489a2934b4793ae021
성동환 <122775098+SungDongHwan@users.noreply.github.com>
2023-02-13 03:19:35 +0900

    ver1.0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-import Taccident from './05/Taccident';
 import Gal from './06/Gal'
-import Mycom from './07/Mycom';
-import Myform from './08/Myform';
+
 const App=()=> {
   return (
   //  <div>
